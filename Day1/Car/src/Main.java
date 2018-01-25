a78c289fee975f420047b1b8bda80e64ca217c65
danielhkim92 <danielhkim92@gmail.com>
2018-01-25 14:31:45 -0600

    eureka, println is working properly

---
+++ Day1/Car/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    int maxSpeed = 100;
+    int minSpeed = 0;
+    double weight = 4079;
+    boolean isTheCarOn = false;
+    char condition = 'A';
+//char is only one character. it is not a string.  it can be a symbol, number, or letter
+
+    public void printVariables() {
+        System.out.println(maxSpeed);
+        System.out.println(minSpeed);
+        System.out.println(weight);
+        System.out.println(isTheCarOn);
+        System.out.println(condition);
+    }
+
+    public static void main(String[] args) {
+        // write your code heres
+        Main carry = new Main();
+        carry.printVariables();
+    }
+}
