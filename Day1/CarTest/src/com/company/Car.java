4a0ac26abe991b5c9d5cc9d95fc96a8ab070bdab
danielhkim92 <danielhkim92@gmail.com>
2018-01-25 14:19:43 -0600

    learning how to print

--- Day1/CarTest/src/com/company/Car.java
+++ Day1/CarTest/src/com/company/Car.java
@@ -2,12 +2,20 @@ package car;
 
 public class Car {
 
-    int maxSpeed;
-    int minSpeed;
-    double weight;
-    boolean isTheCarOn;
-    char condition;
+    int maxSpeed = 100;
+    int minSpeed = 0;
+    double weight = 4079;
+    boolean isTheCarOn = false;
+    char condition = "A";
 //char is only one character. it is not a string.  it can be a symbol, number, or letter
+
+    public void printVariables() {
+        System.out.println(maxSpeed);
+        System.out.println(minSpeed);
+        System.out.println(weight);
+        System.out.println(isTheCarOn);
+        System.out.println(condition);
+    }
 
     public static void main(String[] args) {
 	// write your code heres
