24315bcb6ba4ea8f14486f6b53063f258e5708b9
danielhkim92 <danielhkim92@gmail.com>
2018-01-25 14:14:52 -0600

    established properties

--- Day1/CarTest/src/com/company/Car.java
+++ Day1/CarTest/src/com/company/Car.java
@@ -5,7 +5,9 @@ public class Car {
     int maxSpeed;
     int minSpeed;
     double weight;
-
+    boolean isTheCarOn;
+    char condition;
+//char is only one character. it is not a string.  it can be a symbol, number, or letter
 
     public static void main(String[] args) {
 	// write your code heres
