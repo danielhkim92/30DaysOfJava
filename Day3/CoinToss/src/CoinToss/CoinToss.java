8a537a2b0c9635547627db9971aaa1fac582cb0c
danielhkim92 <danielhkim92@gmail.com>
2018-01-27 16:55:40 -0600

    imported random()

---
+++ Day3/CoinToss/src/CoinToss/CoinToss.java
@@ -0,0 +1,15 @@
+package CoinToss;
+
+import java.util.Random();
+
+public class CoinToss {
+
+    public int tossACoin() {
+        Random rand = new Random();
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
