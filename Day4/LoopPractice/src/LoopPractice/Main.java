909cd8a2b7fb65aeeaef4780a3a2c533d5ea57c5
danielhkim92 <danielhkim92@gmail.com>
2018-01-29 15:53:51 -0600

    difficulty settetting up using intellij

---
+++ Day4/LoopPractice/src/LoopPractice/Main.java
@@ -0,0 +1,16 @@
+package LoopPractice;
+
+public class Main {
+
+    public void practiceWhileLoop() {
+        int x = 0;
+        while(x < 5) {
+            System.out.println("The value of x is  " + x);
+////            x = x + 1;
+            x++;
+        }
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
