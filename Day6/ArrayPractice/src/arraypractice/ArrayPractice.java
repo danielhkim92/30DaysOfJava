9e2610c8842ffe8ef34396c1c8202c3b06bb756c
danielhkim92 <danielhkim92@gmail.com>
2018-01-31 14:19:16 -0600

    declaring, inting, and allocating arrays in java

--- Day6/ArrayPractice/src/arraypractice/ArrayPractice.java
+++ Day6/ArrayPractice/src/arraypractice/ArrayPractice.java
@@ -1,8 +1,22 @@
 package arraypractice;
 
+import java.util.Arrays;
+
 public class ArrayPractice {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //3 ways of initiating an array
+        //declaring
+        int[] intArray1;
+        ////allocating
+        int[] intArray2 = new int[4];
+        ////initializing
+        int[] intArray3 = {5, 2, 9, 1 , 3};
+        ////S
+        String[] stringArray1 =  {"bananas", "apples", "pears"};
+
+        System.out.println(Arrays.toString(intArray2));
     }
 }
