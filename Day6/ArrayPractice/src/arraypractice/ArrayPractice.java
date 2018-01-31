3a12e52c955a293bb2fd1f3ef5168584eccb74a2
danielhkim92 <danielhkim92@gmail.com>
2018-01-31 14:26:15 -0600

    practing writing for loops to print out arrays in java

--- Day6/ArrayPractice/src/arraypractice/ArrayPractice.java
+++ Day6/ArrayPractice/src/arraypractice/ArrayPractice.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 
 public class ArrayPractice {
 
+    public static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            int item = array[i];
+            System.out.print(item);
+            if (i <array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
     public static void main(String[] args) {
 	// write your code here
 
@@ -17,6 +29,16 @@ public class ArrayPractice {
         ////S
         String[] stringArray1 =  {"bananas", "apples", "pears"};
 
+        intArray2[0] = 10;
+        intArray2[1] = 8;
+        intArray3[2] = 5;
+        intArray2[3] = 10;
+
+        // print out arrays
         System.out.println(Arrays.toString(intArray2));
+
+        ///custom print out arrays
+        printArray(intArray2);
+        printArray(intArray3);
     }
 }
